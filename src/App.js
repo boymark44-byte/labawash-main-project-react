266719bcf8bae459aa3ae24ea322d3113b778db1
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2022-08-02 15:53:03 +0800

    Added the Testimonial Component and the styling for it.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import {useState, useEffect} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import NavBar from './components/NavBar'
-import Footer from './components/Footer'
 import Header from './components/Header'
 import About from './components/About'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
 
 
@@ -22,7 +23,7 @@ const App = () => {
         <Route path="/about" element={<About></About>}>About</Route>
       </Routes>
 
-
+      <Testimonials></Testimonials>
       <Footer></Footer>
     </Router>
 
