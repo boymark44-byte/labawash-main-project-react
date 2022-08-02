87eb4608c5b31db4ec90f6e2ed53979ac74cb122
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2022-08-02 12:52:43 +0800

    Added some routes but still not working or redundant.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import About from './components/About'
+
 
 
 const App = () => {
 
   return (
     <Router>
-      
+
       <NavBar></NavBar>
 
       <Header></Header>
       
-      <Footer></Footer>
+      <Routes>
+        
+        <Route path="/about" element={<About></About>}>About</Route>
+      </Routes>
 
-      
+
+      <Footer></Footer>
     </Router>
+
   )
 
 }
