9984f672f18e86694577157293e4c19b5423bd3d
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2022-08-02 12:54:52 +0800

    Fixed the Footer Element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,42 @@
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
+  const playStore = require('../assets/play-store.png')
+  const appStore = require('../assets/app-store.png')
+  const labawashLogo = require('../assets/labawash-logo.png')
+  
   return (
-
     <footer className="footer">
       <div className="container">
 
         <div className="row">
+
           {/* 1st Column */}
           <div className="footer-col-1">
             <h3>Download Our App</h3>
             <p>Download for Android and IOS mobile phone.</p>
 
             <div className="app-logo">
-              <img src="https://www.svgrepo.com/show/223032/playstore.svg" alt="play-store" style={{height: 30, width: 30}}/>
-              <img src="https://www.pngrepo.com/png/352973/180/app-store.png" alt="app-store" style={{height: 30, width: 30}} />
+              <img src={playStore} alt="play-store"/>
+              <img src={appStore} alt="app-store"/>
             </div>
           </div>
 
           {/* 2nd Column */}
           <div className="footer-col-2">
-            <img src="https://www.svgrepo.com/show/287692/laundry-washer.svg" alt="" style={{height: 50, width: 50}}/>
-            <p>Find your Laundry Shop or Use our App to optimize your Landry Shop Business with us
+            <Link to="/"><img src={labawashLogo} alt="labawash-logo" /></Link>
+            <p>Find your Laundry Shop or Use our App 
+              to optimize your Landry Shop Business with us
             </p>
           </div>
 
           {/* 3rd Column */}
           <div className="footer-col-3">
-            <h3>Useful Links</h3>
-            <ul>
-              <li>Blog Post 1</li>
-              <li>Blog Post 2</li>
-              <li>Blog Post 3</li>
-            </ul>
-          </div>
-
-          {/* 4th Column */}
-          <div className="footer-col-4">
             <h3>Follow Us</h3>
             <ul>
-              <li>Facebook</li>
-              <li>Twitter</li>
-              <li>Instagram</li>
-              <li>Youtube</li>
+              <li><img src="https://www.svgrepo.com/show/44069/facebook.svg" alt="Facebook" style={{height: 40, width: 40}}/></li> 
+              <li><img src="https://www.svgrepo.com/show/126780/twitter.svg" alt="Twitter" style={{height: 40, width: 40}} /></li>
+              <li><img src="https://www.svgrepo.com/show/111199/instagram.svg" alt="Instagram" style={{height: 40, width: 40}} /></li>
             </ul>
           </div>
         </div>    
@@ -54,7 +48,6 @@ const Footer = () => {
         </p>
       </div>
     </footer>
-    
   )
 }
 
