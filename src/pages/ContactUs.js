e1802f7fe37f13ea86ef2ab092f3847f1d85a8ba
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2022-08-07 21:06:00 +0800

    Added a classname for section and container in the Contact Us Page.

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom'
 
 const ContactUs = () => {
   return (
-    <div className="container">
-        <h2>Contact Us</h2>
-        <p>Contact details.......</p>
-        <Link to='/'>Go back to home.</Link>
-    </div>
+    <section className="contact-us">
+      <div className="small-container">
+          <h2>Contact Us</h2>
+          <p>Contact details.......</p>
+          <Link to='/'>Go back to home.</Link>
+      </div>
+    </section>
   )
 }
 
