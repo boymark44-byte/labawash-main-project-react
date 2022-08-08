f028a9b0238fcf2a02b77e49801a25d6cde276e0
Mark Glenn M. Gabuya <markglenn.gabuya@g.msuiit.edu.ph>
2022-08-08 10:48:34 +0800

    Created and Added the Login Page.

---
+++ src/pages/Login.js
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    
+  return (
+    <div className="account-page">
+        <div className="container">
+            <div className="row">
+                <div className="col-2">
+                    <img src="https://estaticos.qdq.com/swdata/cache/5e/38/5e38279a096ecea015e524b9018675b3.jpg" style={{height: 550, width: 800}}/>
+                </div>
+
+                <div className="col-2">
+                    <div className="form-container-login">
+                        <div className="form-btn">
+                            <span onClick={window['login']}>Login</span>
+                            <hr id="Indicator" />
+                        </div>
+
+                        <form id="LoginForm" style={{textAlign: 'left'}}>
+                            
+                            <label for='email'>Email:</label>
+                            <input type="text" placeholder="Username" name="email"/>
+
+                            <label for='Password'>Password:</label>
+                            <input type="password" placeholder="Password" name="password"/>
+
+                            <button type="submit" className="btn" style={{marginBottom: 20}}>Login</button>
+
+                            
+                        </form>
+                        <Link to='#'>Forgot Password?</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
