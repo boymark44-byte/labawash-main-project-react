a6b7e13980af88f8029de0e51f6bb30285846be7
Dale Jann Paguntalan <dalejann.paguntalan@g.msuiit.edu.ph>
2022-08-09 10:33:37 +0800

    Shop Feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import "./laundry_shops/Shop.css";
 import Home from './pages/Home'
 import About from './pages/About'
 import Error from './pages/Error'
@@ -10,8 +10,26 @@ import ContactUs from './pages/ContactUs'
 import LaundryShops from './laundry_shops/LaundryShops'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import data from './data'
+import Shop from './laundry_shops/Shop'
+import Shops from './laundry_shops/Shops'
+import ShopLayout from './laundry_shops/ShopLayout'
 
 const App = () => {
+
+  const cards = data.map((item) => {
+    return (
+      <Shops
+        id={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
 
   return (
     <Router>
@@ -25,7 +43,12 @@ const App = () => {
           <Route path='/laundry-shops' element={<LaundryShops>Laundry Shops</LaundryShops>}></Route>
           <Route path='*' element={<Error></Error>}>Error</Route>
         </Route>
+        <Route path="/shops" element={<ShopLayout></ShopLayout>}>
+          <Route exact path="/shops" element={cards} />
+          <Route exact path="/shops/:id" element={<Shop />} />
+        </Route>
       </Routes>
+          
     </Router>
   )
 
